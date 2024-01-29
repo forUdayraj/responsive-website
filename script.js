a37d48a019def0b17b14cc02e1bf0bfbc493b3eb
Udayraj Ghuge <92646347+forUdayraj@users.noreply.github.com>
2024-01-30 01:30:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let arr = [
+    {name:"udayraj",age: 23},
+    {name:"Rajvardhan",age: 20},
+    {name:"shivam",age: 22},
+    {name:"pratik",age: 24},
+    {name:"atharv",age: 23},
+    {name:"raj",age: 23}
+]
+
+let newarr = arr.map({})
